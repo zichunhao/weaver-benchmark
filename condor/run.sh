d704daca131b2e8508d035b0a87c1a5d1642a0e5
zichunhao <zichunh2000@gmail.com>
2024-08-01 03:30:40 +0200

    Try two GPUs

--- condor/run.sh
+++ condor/run.sh
@@ -28,35 +28,37 @@ git clone -b GloParT --recursive https://github.com/zichunhao/weaver-benchmark.g
 cd "${WEAVER_PATH}"
 mkdir output
 
+DATA_TAG=20230504_ak8_UL17_v8  # the original dataset
+# DATA_TAG=20230504_ak8_UL17_v8_ext1  # the high-mass extended dataset
+DATA_PATH=/mldata/licq/deepjetak8
+DATA_PATH_IFR=/data/bond/licq/deepjetak8
+config=${WEAVER_PATH}/data_new/inclv7plus/${PREFIX%%.*}.yaml
+PREFIX=ak8_MD_inclv8_part_splitreg_addltphp_wmeasonly_manual.useamp.large_fc2048.gm5.ddp-bs256-lr${lr}
 
 # Training
 lr='5e-4'
-PREFIX=ak8_MD_inclv8_part_splitreg_addltphp_wmeasonly_manual.useamp.large_fc2048.gm5.ddp-bs256-lr${lr}
-config=${WEAVER_PATH}/data_new/inclv7plus/${PREFIX%%.*}.yaml
-DATAPATH=/mldata/licq/deepjetak8
-DATAPATHIFR=/data/bond/licq/deepjetak8
+NGPUS=2
 
-NGPUS=3
-CUDA_VISIBLE_DEVICES=0,1,3 torchrun --standalone --nnodes=1 --nproc_per_node=$NGPUS \
+CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nnodes=1 --nproc_per_node=$NGPUS \
 ${WEAVER_PATH}/train.py \
 --train-mode hybrid -o three_coll True -o loss_split_reg True -o loss_gamma 5 \
 -o fc_params '[(2048,0.1)]'  -o embed_dims '[256,1024,256]' -o pair_embed_dims '[128,128,128]' -o num_heads 16 \
 --use-amp --batch-size 256 --start-lr $lr --num-epochs 50 --optimizer ranger --fetch-step 0.00666667 \
 --backend nccl \
 --data-train \
-$DATAPATH'/20230504_ak8_UL17_v8/QCD_Pt_170toInf_ptBinned_TuneCP5_13TeV_pythia8/*.root' \
-$DATAPATH'/20230504_ak8_UL17_v8/Spin0ToTT_VariableMass_WhadOrlep_MX-600to6000_MH-15to250/*.root' \
-$DATAPATH'/20230504_ak8_UL17_v8/BulkGravitonToHHTo4QGluLTau_MX-600to6000_MH-15to250/*.root' \
-$DATAPATH'/20230504_ak8_UL17_v8/DiH1OrHpm_2HDM_HpmToBC_HpmToCS_H1ToBS_HT-600to6000_MH-15to250/*.root' \
-$DATAPATH'/20230504_ak8_UL17_v8/BulkGravitonToHHTo4W_MX-600to6000_MH-15to250_JHUVariableWMass/*.root' \
-$DATAPATH'/20230504_ak8_UL17_v8/BulkGravitonToHHTo4W_MX-600to6000_MH-15to250_JHUVariableWMass2DMesh/*.root' \
-$DATAPATH'/20230504_ak8_UL17_v8/BulkGravitonToHHTo4Z_MX-600to6000_MH-15to250_JHUVariableZMass/*.root' \
-$DATAPATH'/20230504_ak8_UL17_v8/BulkGravitonToHHTo4Z_MX-600to6000_MH-15to250_JHUVariableZMass2DMesh/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'QCD_Pt_170toInf_ptBinned_TuneCP5_13TeV_pythia8/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'Spin0ToTT_VariableMass_WhadOrlep_MX-600to6000_MH-15to250/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'BulkGravitonToHHTo4QGluLTau_MX-600to6000_MH-15to250/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'DiH1OrHpm_2HDM_HpmToBC_HpmToCS_H1ToBS_HT-600to6000_MH-15to250/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'BulkGravitonToHHTo4W_MX-600to6000_MH-15to250_JHUVariableWMass/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'BulkGravitonToHHTo4W_MX-600to6000_MH-15to250_JHUVariableWMass2DMesh/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'BulkGravitonToHHTo4Z_MX-600to6000_MH-15to250_JHUVariableZMass/*.root' \
+"${DATA_PATH}/${DATA_TAG}"/'BulkGravitonToHHTo4Z_MX-600to6000_MH-15to250_JHUVariableZMass2DMesh/*.root' \
 --data-test \
-'hww:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/GluGluToBulkGravitonToHHTo4W_JHUGen_M-1000_narrow/*.root' \
-'higgs2p:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/GluGluToBulkGravitonToHHTo4QGluLTau_M-1000_narrow/*.root' \
-'qcd:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/QCD_Pt_170toInf_ptBinned_TuneCP5_13TeV_pythia8/*.root' \
-'ttbar:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/ZprimeToTT_M1200to4500_W12to45_TuneCP2_PSweights/*.root' \
+'hww:'"${DATA_PATH_IFR}/${DATA_TAG}"/'infer/GluGluToBulkGravitonToHHTo4W_JHUGen_M-1000_narrow/*.root' \
+'higgs2p:'"${DATA_PATH_IFR}/${DATA_TAG}"/'infer/GluGluToBulkGravitonToHHTo4QGluLTau_M-1000_narrow/*.root' \
+'qcd:'"${DATA_PATH_IFR}/${DATA_TAG}"/'infer/QCD_Pt_170toInf_ptBinned_TuneCP5_13TeV_pythia8/*.root' \
+'ttbar:'"${DATA_PATH_IFR}/${DATA_TAG}"/'infer/ZprimeToTT_M1200to4500_W12to45_TuneCP2_PSweights/*.root' \
 --samples-per-epoch $((15000 * 512 / $NGPUS)) --samples-per-epoch-val $((1000 * 512)) \
 --data-config ${config} --num-workers 8 \
 --network-config ${WEAVER_PATH}/networks/example_ParticleTransformer2023Tagger_hybrid.py \
@@ -71,13 +73,13 @@ python ${WEAVER_PATH}/train.py --predict --gpus 1 \
 -o fc_params '[(2048,0.1)]'  -o embed_dims '[256,1024,256]' -o pair_embed_dims '[128,128,128]' -o num_heads 16 \
 --use-amp --batch-size 256 --start-lr 1e-3 --num-epochs 50 --optimizer ranger --fetch-step 0.00666667 \
 --data-test \
-'xww:'$DATAPATHIFR'/20230504_ak8_UL17_v8/BulkGravitonToHHTo4W_MX-600to6000_MH-15to250_JHUVariableWMass/*0.root' \
-'hww:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/GluGluToBulkGravitonToHHTo4W_JHUGen_M-1000_narrow/*.root' \
-'higgs2p:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/GluGluToBulkGravitonToHHTo4QGluLTau_M-1000_narrow/*.root' \
-'qcd:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/QCD_Pt_170toInf_ptBinned_TuneCP5_13TeV_pythia8/*.root' \
-'ttbar:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/ZprimeToTT_M1200to4500_W12to45_TuneCP2_PSweights/*.root' \
-'ofcttbarfl:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/*.root' \
-'ofcttbarsl:'$DATAPATHIFR'/20230504_ak8_UL17_v8/infer/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*.root' \
+'xww:'"$DATA_PATH_IFR/${DATA_TAG}"'/BulkGravitonToHHTo4W_MX-600to6000_MH-15to250_JHUVariableWMass/*0.root' \
+'hww:'"$DATA_PATH_IFR/${DATA_TAG}"'/infer/GluGluToBulkGravitonToHHTo4W_JHUGen_M-1000_narrow/*.root' \
+'higgs2p:'"$DATA_PATH_IFR/${DATA_TAG}"'/infer/GluGluToBulkGravitonToHHTo4QGluLTau_M-1000_narrow/*.root' \
+'qcd:'"$DATA_PATH_IFR/${DATA_TAG}"'/infer/QCD_Pt_170toInf_ptBinned_TuneCP5_13TeV_pythia8/*.root' \
+'ttbar:'"$DATA_PATH_IFR/${DATA_TAG}"'/infer/ZprimeToTT_M1200to4500_W12to45_TuneCP2_PSweights/*.root' \
+'ofcttbarfl:'"$DATA_PATH_IFR/${DATA_TAG}"'/infer/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/*.root' \
+'ofcttbarsl:'"$DATA_PATH_IFR/${DATA_TAG}"'/infer/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*.root' \
 --samples-per-epoch $((15000 * 512 / $NGPUS)) --samples-per-epoch-val $((1000 * 512)) \
 --data-config ${config} --num-workers 8 \
 --network-config ${WEAVER_PATH}/networks/example_ParticleTransformer2023Tagger_hybrid.py \
